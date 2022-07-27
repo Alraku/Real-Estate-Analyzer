664ce1b907d93ef2a3f31b4a8f2aead834ea7e10
Alraku <ekoalraku@gmail.com>
2022-07-27 21:55:32 +0200

    Fixed gathering data, added simple analisys

--- source/main.py
+++ source/main.py
@@ -1,10 +1,9 @@
-from pprint import pprint
 from html_finder import HTMLFinder
 from xpath_finder import XpathFinder
 from request import make_request
 
-from pprint import pprint
 import json
+
 
 URL = 'https://www.olx.pl/sport-hobby/rowery/rowery-gorskie/q-marlin-7/'
 
@@ -14,18 +13,28 @@ def main():
 
     finder = HTMLFinder(('div', 'offer-wrapper'))
     finder.feed(str(html_response))
-    # finder.save_data()
+    finder.save_data()
 
     xpath = XpathFinder()
-    table = finder.get_data()
-    for offer in range(10):
-        dict = {
-            'title': next(xpath.find_by_xpath("//a/strong/text()")),
-            'price': next(xpath.find_by_xpath("//p[@class='price']/strong/text()"))
-        }
+    zmienna = xpath.find_by_xpath("//a/strong/text()")
+    zmienna2 = xpath.find_by_xpath("//p[@class='price']/strong/text()")
+    zmienna3 = xpath.find_by_xpath("///i[@data-icon='location-filled']/following-sibling::text()")
+    list = []
+    
+    for offer in finder.get_data():
+        dict = {}
+        dict['title'] = next(zmienna)
+        dict['price'] = next(zmienna2).replace(' ', '').replace('zł', '')
+        dict['location'] = next(zmienna3)
+        list.append(dict)
      
-    with open('result.json', 'w') as file:
-        json.dump(dict, file)
+    
+    with open('result.json', 'w', encoding='utf8') as file:
+        json.dump(list, file, ensure_ascii=False)
+
+    prices = [int(dict['price']) for dict in list if 'price' in dict]
+    print(sum(prices) / len(list))
+    
 
 
 if __name__ == "__main__":
