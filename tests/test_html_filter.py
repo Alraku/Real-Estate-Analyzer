7f031ce0e9be99997ec568c92f66a568e55a11c2
Alraku <ekoalraku@gmail.com>
2022-08-03 21:47:47 +0200

    New tests file

---
+++ tests/test_html_filter.py
@@ -0,0 +1,17 @@
+from source.html_filter import HTMLFilter
+
+
+def test_html_filter():
+    HTMLFilter_instance = HTMLFilter(('div', 'second'))
+    HTMLFilter_instance.feed('<div class="test"><span itemprop="description"><div class="first second"><h1>My First Heading<p>Banino</p></h1></div><p>My first <br/><br/>paragraph.</p></span></div>')
+    actual = HTMLFilter_instance.get_data()
+    expected = '<h1>My First Heading<p>Banino</p></h1>'
+    assert expected in actual, f'Assertion Failed, actual = {actual}, expected = {expected}'
+
+
+def test_html_filter_2():
+    HTMLFilter_instance = HTMLFilter(('div', 'test'))
+    HTMLFilter_instance.feed('<div class="test"><span itemprop="description"><h1>My First Heading</h1><p>My first <br/><br/>paragraph.</p></span></div>')
+    actual = HTMLFilter_instance.get_data()
+    expected = '<span itemprop="description"><h1>My First Heading</h1><p>My first <br/><br/>paragraph.</p></span>'
+    assert expected in actual, f'Assertion Failed, actual = {actual}, expected = {expected}'
