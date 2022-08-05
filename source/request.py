ff8e5bf3b0575838d3c7afc9b705e86f3b59d4a1
Alraku <ekoalraku@gmail.com>
2022-08-05 21:54:27 +0200

    Moved headers out of request function

--- source/request.py
+++ source/request.py
@@ -1,22 +1,18 @@
 from urllib.request import urlopen
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
-
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
-    'Content-Type': 'text/html; charset=utf-8'
-    }
+from source.globals import HEADERS
 
 
 def make_request(URL: str) -> str:
     try:
         request = Request(URL, headers=HEADERS)
         response = urlopen(request)
-        html_response = response.read().decode("utf-8")
-        return html_response
-        
-    except HTTPError as e:
-        print(e.status, e.reason)
-    except URLError as e:
-        print(e.reason)
+        return decode_utf8(response)
+
+    except (HTTPError, URLError) as error:
+        print(error.status, error.reason)
+
+
+def decode_utf8(response: str) -> str:
+    return response.read().decode("utf-8")
