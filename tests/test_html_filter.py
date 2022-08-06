112c543a5c7578df911e7b11587eccf2c92112b1
Alraku <ekoalraku@gmail.com>
2022-08-06 21:25:30 +0200

    Added new test

--- tests/test_html_filter.py
+++ tests/test_html_filter.py
@@ -15,3 +15,11 @@ def test_html_filter_2():
     actual = HTMLFilter_instance.get_data()
     expected = '<span itemprop="description"><h1>My First Heading</h1><p>My first <br/><br/>paragraph.</p></span>'
     assert expected in actual, f'Assertion Failed, actual = {actual}, expected = {expected}'
+
+
+def test_save_to_file():
+
+    HTMLFilter_instance = HTMLFilter(('div', 'test'))
+    HTMLFilter_instance.feed('<div class="test"><span itemprop="description"><h1>My First Heading</h1><p>My first <br/><br/>paragraph.</p></span></div>')
+    HTMLFilter_instance.save_data()
+    assert True
