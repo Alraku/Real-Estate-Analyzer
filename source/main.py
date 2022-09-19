1d0493812ce2fa081a112d9fc6af1a1a67b935ec
Alraku <ekoalraku@gmail.com>
2022-09-19 21:38:12 +0200

    Changed values of test data

--- source/main.py
+++ source/main.py
@@ -1,5 +1,5 @@
 from request import make_request
-from html_finder import HTMLFilter
+from html_filter import HTMLFilter
 from xpath_finder import XpathFinder
 
 
@@ -7,24 +7,24 @@ def scrap_data():
 
     URL = 'https://www.olx.pl/sport-hobby/rowery/rowery-gorskie/q-marlin-5/'
 
-    finder = HTMLFilter(('div', 'css-9nzgu8'))
+    finder = HTMLFilter(('div', 'offer-wrapper'))
     finder.feed(make_request(URL))
     finder.save_data()
 
     values_to_find = {
-        'title': '//h6[@class="css-v3vynn-Text eu5v0x0"]/text()',
-        'price': '//p[@data-testid="ad-price"]/text()',
-        'location': '//p[@data-testid="location-date"]/text()'
+        'title': "//a/strong/text()",
+        'price': "//p[@class='price']/strong/text()",
+        'location': "///i[@data-icon='location-filled']/following-sibling::text()"
     }
 
     test_var = XpathFinder().xpath(values_to_find).get()
-    print(type(test_var), test_var)
+    # print(type(test_var), test_var)
 
 
-    next_page = XpathFinder().xpath({'next_page_button': '//h6[@class="css-v3vynn-Text eu5v0x0"]/text()'}).get()
-    print(next_page)
-    if next_page is not None:
-        pass
+    # next_page = XpathFinder().xpath({'next_page_button': '//h6[@class="css-v3vynn-Text eu5v0x0"]/text()'}).get()
+    # print(next_page)
+    # if next_page is not None:
+    #     pass
         #TODO Sprawić tal, aby nowa instancja XPATHFINDER nie korzystała z htmla przefiltrowanego tylko bazowego.
         #TODO Sprawić aby mozna było konkretne atrybuty (np. href) pobierać i wypisywać.
         # next_page = response.urljoin(next_page)
